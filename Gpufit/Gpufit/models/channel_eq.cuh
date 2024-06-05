8953795f3c7a235534f7c6d53b541a2693a1b48a
Kexin Zheng <kexinz0227@g.ucla.edu>
2024-06-05 15:51:19 -0700

    completed the model for channel equation
    need to test it

--- Gpufit/Gpufit/models/channel_eq.cuh
+++ Gpufit/Gpufit/models/channel_eq.cuh
@@ -1,54 +1,29 @@
-#ifndef CAHNNEL_EQ_CUH_INCLUDED
-#define CAHNNEL_EQ_CUH_INCLUDED
-/* Description of the calculate_gauss2d function
-* ==============================================
-*
-* This function calculates the values of two-dimensional gauss model functions
-* and their partial derivatives with respect to the model parameters. 
-*
-* No independent variables are passed to this model function.  Hence, the 
-* (X, Y) coordinate of the first data value is assumed to be (0.0, 0.0).  For
-* a fit size of M x N data points, the (X, Y) coordinates of the data are
-* simply the corresponding array index values of the data array, starting from
-* zero.
-*
-* Parameters:
-*
-* parameters: An input vector of model parameters.
-*             p[0]: amplitude
-*             p[1]: center coordinate x
-*             p[2]: center coordinate y
-*             p[3]: width (standard deviation; equal width in x and y dimensions)
-*             p[4]: offset
-*
-* n_fits: The number of fits. (not used)
-*
-* n_points: The number of data points per fit.
-*
-* value: An output vector of model function values.
-*
-* derivative: An output vector of model function partial derivatives.
-*
-* point_index: The data point index.
-*
-* fit_index: The fit index. (not used)
-*
-* chunk_index: The chunk index. (not used)
-*
-* user_info: An input vector containing user information. (not used)
-*
-* user_info_size: The size of user_info in bytes. (not used)
-*
-* Calling the calculate_gauss2d function
-* ======================================
-*
-* This __device__ function can be only called from a __global__ function or an other
-* __device__ function.
-*
-*/
+#include <math.h>
 
-__device__ void calculate_channel_eq (               // ... = function name
-    float const * parameters,
+#ifndef GPUFIT_CHANNEL_EQ_CUH_INCLUDED
+#define GPUFIT_CHANNEL_EQ_CUH_INCLUDED
+
+/***
+ * parameters:
+ *  tau, nu, h_real, h_imaginary
+ *  p[0], p[1], p[2], p[3]
+ * 
+ * n_fits (number of fits)
+ *  number of estimation tasks to be performed at 1 time (custom)
+ * 
+ * n_points (number of data per fit)
+ *  pilot number
+ * 
+ * value
+ *  estimated y
+ * 
+ * user_info
+ *  m, n, y_real, y_imaginary
+ * 
+ * 
+*/
+__device__ void channel_eq (
+    REAL const * parameters,
     int const n_fits,
     int const n_points,
     float * value,
@@ -59,32 +34,63 @@ __device__ void calculate_channel_eq (               // ... = function name
     char * user_info,
     std::size_t const user_info_size)
 {
-    // indices
+    ///////////////////////////// modify user_info //////////////////////////////
+    REAL * user_info_float = (REAL*)user_info;
+    REAL * m, n= 0;
 
-    int const n_points_x = sqrt((REAL)n_points);
-    int const point_index_y = point_index / n_points_x;
-    int const point_index_x = point_index - point_index_y * n_points_x;
-
-    // parameters
-
-    REAL const * p = parameters;
+    int const chunk_begin = chunk_index * n_fits * n_points * 2;
+    int const fit_begin = fit_index * n_points;
+    m = user_info_float[chunk_begin + fit_begin + point_index * 2];
+    n = user_info_float[chunk_begin + fit_begin + point_index * 2 + 1];
 
     ///////////////////////////// value //////////////////////////////
 
-    REAL const argx = (point_index_x - p[1]) * (point_index_x - p[1]) / (2 * p[3] * p[3]);
-    REAL const argy = (point_index_y - p[2]) * (point_index_y - p[2]) / (2 * p[3] * p[3]);
-    REAL const ex = exp(-(argx + argy));
-    value[point_index] = p[0] * ex + p[4];  // formula calculating fit model values
+    REAL const A = cos(2 * M_PI * (m * p[0] - n * p[1]));
+    REAL const B = sin(2 * M_PI * (m * p[0] - n * p[1]));
+
+    // calculate estimated y
+    value[point_index * 2] = A * p[2] - B * p[3];              // real part of estimated y
+    value[point_index * 2 + 1] = A * p[3] + B * p[2];          // imaginary part of estimated y
 
     /////////////////////////// derivative ///////////////////////////
-
     REAL * current_derivative = derivative + point_index;
 
-    current_derivative[0 * n_points] = ex;  // formula calculating derivative values with respect to parameters[0]
-    current_derivative[1 * n_points] = p[0] * ex * (point_index_x - p[1]) / (p[3] * p[3]);
-    current_derivative[2 * n_points] = p[0] * ex * (point_index_y - p[2]) / (p[3] * p[3]);
-    current_derivative[3 * n_points] = ex * p[0] * ((point_index_x - p[1]) * (point_index_x - p[1]) + (point_index_y - p[2]) * (point_index_y - p[2])) / (p[3] * p[3] * p[3]);
-    current_derivative[4 * n_points] = 1;
+    // derivative of real part of y
+
+    // derivative wrt tau
+    for (int i = 0; i < 3; i++)
+        current_derivative[i * 2 * n_points] = -2 * M_PI * m * B * p[2] - 2 * M_PI * m * A * p[3]; 
+
+    // derivative wrt nu
+    for (int i = 0; i < 3; i++)
+        current_derivative[(i + 3) * 2 * n_points ] = 2 * M_PI * n * B * p[2] + 2 * M_PI * n * A * p[3]; 
+    
+    // derivative wrt h_real
+    for (int i = 0; i < 3; i++)
+        current_derivative[(i + 6) * 2  * n_points] = A; 
+
+    // derivative wrt h_img
+        // derivative wrt h_real
+    for (int i = 0; i < 3; i++)
+        current_derivative[(i + 9) * 2  * n_points] = -B; 
+
+    // derivative of imaginary part of y
+
+        // derivative wrt tau
+    for (int i = 0; i < 3; i++)
+        current_derivative[(2 * i - 1) * n_points] = -2 * M_PI * m * B * p[3] + 2 * M_PI * m * A * p[2]; 
+
+    // derivative wrt nu
+    for (int i = 0; i < 3; i++)
+        current_derivative[(2 * i + 5) * n_points] = 2 * M_PI * n * B * p[3] - 2 * M_PI * n * A * p[2]; 
+    
+    // derivative wrt h_real
+    for (int i = 0; i < 3; i++)
+        current_derivative[(2 * i + 11) * n_points] = A; 
+
+    // derivative wrt h_img
+    for (int i = 0; i < 3; i++)
+        current_derivative[(2 * i + 17) * n_points] = B; 
 }
 
 #endif
