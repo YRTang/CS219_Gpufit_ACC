672ed2b59419e6e007b7fe3e6a7b771d7dc12744
YRTang <sarahtyr09@outlook.com>
2024-06-05 16:32:26 -0700

    gpu estimator

--- Gpufit/Gpufit/estimators/estimators.cuh
+++ Gpufit/Gpufit/estimators/estimators.cuh
@@ -3,16 +3,17 @@
 
 #include "lse.cuh"
 #include "mle.cuh"
+#include "lse_complex.cuh"
 
 __device__ void calculate_chi_square(
     int const estimator_id,
-    volatile REAL * chi_square,
+    volatile REAL *chi_square,
     int const point_index,
-    REAL const * data,
-    REAL const * value,
-    REAL const * weight,
-    int * state,
-    char * user_info,
+    REAL const *data,
+    REAL const *value,
+    REAL const *weight,
+    int *state,
+    char *user_info,
     std::size_t const user_info_size)
 {
     switch (estimator_id)
@@ -23,6 +24,9 @@ __device__ void calculate_chi_square(
     case MLE:
         calculate_chi_square_mle(chi_square, point_index, data, value, weight, state, user_info, user_info_size);
         break;
+    case LSE_COMPLEX:
+        calculate_chi_square_lse_complex(chi_square, point_index, data, value, weight, state, user_info, user_info_size);
+        break;
     default:
         assert(0); // unknown estimator ID
     }
@@ -30,14 +34,14 @@ __device__ void calculate_chi_square(
 
 __device__ void calculate_gradient(
     int const estimator_id,
-    volatile REAL * gradient,
+    volatile REAL *gradient,
     int const point_index,
     int const parameter_index,
-    REAL const * data,
-    REAL const * value,
-    REAL const * derivative,
-    REAL const * weight,
-    char * user_info,
+    REAL const *data,
+    REAL const *value,
+    REAL const *derivative,
+    REAL const *weight,
+    char *user_info,
     std::size_t const user_info_size)
 {
     switch (estimator_id)
@@ -48,6 +52,9 @@ __device__ void calculate_gradient(
     case MLE:
         calculate_gradient_mle(gradient, point_index, parameter_index, data, value, derivative, weight, user_info, user_info_size);
         break;
+    case LSE_COMPLEX:
+        calculate_gradient_lse_complex(gradient, point_index, parameter_index, data, value, derivative, weight, user_info, user_info_size);
+        break;
     default:
         assert(0); // unknown estimator ID
     }
@@ -55,26 +62,27 @@ __device__ void calculate_gradient(
 
 __device__ void calculate_hessian(
     int const estimator_id,
-    double * hessian,
+    double *hessian,
     int const point_index,
     int const parameter_index_i,
     int const parameter_index_j,
-    REAL const * data,
-    REAL const * value,
-    REAL const * derivative,
-    REAL const * weight,
-    char * user_info,
+    REAL const *data,
+    REAL const *value,
+    REAL const *derivative,
+    REAL const *weight,
+    char *user_info,
     std::size_t const user_info_size)
 {
     switch (estimator_id)
     {
     case LSE:
-        calculate_hessian_lse
-        (hessian, point_index, parameter_index_i, parameter_index_j, data, value, derivative, weight, user_info,user_info_size);
+        calculate_hessian_lse(hessian, point_index, parameter_index_i, parameter_index_j, data, value, derivative, weight, user_info, user_info_size);
         break;
     case MLE:
-        calculate_hessian_mle
-        (hessian, point_index, parameter_index_i, parameter_index_j, data, value, derivative, weight, user_info, user_info_size);
+        calculate_hessian_mle(hessian, point_index, parameter_index_i, parameter_index_j, data, value, derivative, weight, user_info, user_info_size);
+        break;
+    case LSE_COMPLEX:
+        calculate_hessian_lse_complex(hessian, point_index, parameter_index_i, parameter_index_j, data, value, derivative, weight, user_info, user_info_size);
         break;
     default:
         assert(0); // unknown estimator ID
