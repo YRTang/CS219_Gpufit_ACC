f31374a2774e4b37493f99d24638485c7bc1521c
YRTang <sarahtyr09@outlook.com>
2024-06-05 18:29:58 -0700

    gpu-test-bugs

--- Gpufit/Gpufit/models/channel_eq.cuh
+++ Gpufit/Gpufit/models/channel_eq.cuh
@@ -7,36 +7,37 @@
  * parameters:
  *  tau, nu, h_real, h_imaginary
  *  p[0], p[1], p[2], p[3]
- * 
+ *
  * n_fits (number of fits)
  *  number of estimation tasks to be performed at 1 time (custom)
- * 
+ *
  * n_points (number of data per fit)
  *  pilot number
- * 
+ *
  * value
  *  estimated y
- * 
+ *
  * user_info
  *  m, n, y_real, y_imaginary
- * 
- * 
-*/
-__device__ void channel_eq (
-    REAL const * parameters,
+ *
+ *
+ */
+__device__ void calculate_channel_eq(
+    REAL const *parameters,
     int const n_fits,
     int const n_points,
-    float * value,
-    float * derivative,
+    float *value,
+    float *derivative,
     int const point_index,
     int const fit_index,
     int const chunk_index,
-    char * user_info,
+    char *user_info,
     std::size_t const user_info_size)
 {
     ///////////////////////////// modify user_info //////////////////////////////
-    REAL * user_info_float = (REAL*)user_info;
-    REAL * m, n= 0;
+    REAL *user_info_float = (REAL *)user_info;
+    REAL m, n = 0;
+    REAL const *p = parameters;
 
     int const chunk_begin = chunk_index * n_fits * n_points * 2;
     int const fit_begin = fit_index * n_points;
@@ -49,48 +50,48 @@ __device__ void channel_eq (
     REAL const B = sin(2 * M_PI * (m * p[0] - n * p[1]));
 
     // calculate estimated y
-    value[point_index * 2] = A * p[2] - B * p[3];              // real part of estimated y
-    value[point_index * 2 + 1] = A * p[3] + B * p[2];          // imaginary part of estimated y
+    value[point_index * 2] = A * p[2] - B * p[3];     // real part of estimated y
+    value[point_index * 2 + 1] = A * p[3] + B * p[2]; // imaginary part of estimated y
 
     /////////////////////////// derivative ///////////////////////////
-    REAL * current_derivative = derivative + point_index;
+    REAL *current_derivative = derivative + point_index;
 
     // derivative of real part of y
 
     // derivative wrt tau
     for (int i = 0; i < 3; i++)
-        current_derivative[i * 2 * n_points] = -2 * M_PI * m * B * p[2] - 2 * M_PI * m * A * p[3]; 
+        current_derivative[i * 2 * n_points] = -2 * M_PI * m * B * p[2] - 2 * M_PI * m * A * p[3];
 
     // derivative wrt nu
     for (int i = 0; i < 3; i++)
-        current_derivative[(i + 3) * 2 * n_points ] = 2 * M_PI * n * B * p[2] + 2 * M_PI * n * A * p[3]; 
-    
+        current_derivative[(i + 3) * 2 * n_points] = 2 * M_PI * n * B * p[2] + 2 * M_PI * n * A * p[3];
+
     // derivative wrt h_real
     for (int i = 0; i < 3; i++)
-        current_derivative[(i + 6) * 2  * n_points] = A; 
+        current_derivative[(i + 6) * 2 * n_points] = A;
 
     // derivative wrt h_img
-        // derivative wrt h_real
+    // derivative wrt h_real
     for (int i = 0; i < 3; i++)
-        current_derivative[(i + 9) * 2  * n_points] = -B; 
+        current_derivative[(i + 9) * 2 * n_points] = -B;
 
     // derivative of imaginary part of y
 
-        // derivative wrt tau
+    // derivative wrt tau
     for (int i = 0; i < 3; i++)
-        current_derivative[(2 * i - 1) * n_points] = -2 * M_PI * m * B * p[3] + 2 * M_PI * m * A * p[2]; 
+        current_derivative[(2 * i - 1) * n_points] = -2 * M_PI * m * B * p[3] + 2 * M_PI * m * A * p[2];
 
     // derivative wrt nu
     for (int i = 0; i < 3; i++)
-        current_derivative[(2 * i + 5) * n_points] = 2 * M_PI * n * B * p[3] - 2 * M_PI * n * A * p[2]; 
-    
+        current_derivative[(2 * i + 5) * n_points] = 2 * M_PI * n * B * p[3] - 2 * M_PI * n * A * p[2];
+
     // derivative wrt h_real
     for (int i = 0; i < 3; i++)
-        current_derivative[(2 * i + 11) * n_points] = A; 
+        current_derivative[(2 * i + 11) * n_points] = A;
 
     // derivative wrt h_img
     for (int i = 0; i < 3; i++)
-        current_derivative[(2 * i + 17) * n_points] = B; 
+        current_derivative[(2 * i + 17) * n_points] = B;
 }
 
 #endif
--- Gpufit/examples/c++/myProject/mp_solver_example.cpp
+++ Gpufit/examples/c++/myProject/mp_solver_example.cpp
@@ -1,4 +1,4 @@
-#include "/home/test/sarahTang/Gpufit/Gpufit/gpufit.h"
+#include "../../../Gpufit/gpufit.h"
 #include "mp_solver.h"
 #include "csv_reader.h"
 #include <vector>
@@ -6,47 +6,53 @@
 #include <iostream>
 #include <math.h>
 
-#define CSV_PATH "/home/test/sarahTang/Gpufit/examples/c++/myProject/sample_input.csv"
+#define CSV_PATH "../Gpufit/examples/c++/myProject/sample_input.csv"
 
 void mp_solver_example(mp_profile_t *mp_profile, mp_config_t *mp_config)
 {
 	// number of fits, fit points and parameters
-	std::size_t const n_fits = 1000;
+	std::size_t const n_fits = 1;
 	std::size_t const n_points_per_fit = mp_config->nof_pilots; //=32
 	std::size_t const n_model_parameters = 12;
 
 	// custom x positions for the data points of every fit, stored in user info
-	std::vector< REAL > user_info(n_points_per_fit*2);
-	
+	std::vector<REAL> user_info(n_points_per_fit * 2);
+
 	for (std::size_t i = 0; i < n_points_per_fit; i++)
 	{
-		user_info[i*2] = mp_config->m[i];
-		user_info[i*2+1] = mp_config->n[i];
+		user_info[i * 2] = mp_config->m[i];
+		user_info[i * 2 + 1] = mp_config->n[i];
 	}
 
 	// size of user info in bytes
-	std::size_t const user_info_size = 2 * n_points_per_fit * sizeof(REAL); 
+	std::size_t const user_info_size = 2 * n_points_per_fit * sizeof(REAL);
 
 	// initial parameters (randomized)
-	std::vector< REAL > initial_parameters(n_fits * n_model_parameters);
+	std::vector<REAL> initial_parameters(n_fits * n_model_parameters);
 	for (std::size_t i = 0; i != n_fits; i++)
 	{
 		for (int p = 0; p < mp_config->nof_paths; ++p)
 		{
-			initial_parameters[i * n_model_parameters + p*4 + 0] =  mp_profile->tau[p]; //4: t, v, h_real, h_imag
-			initial_parameters[i * n_model_parameters + p*4 + 1] =  mp_profile->nu[p];
-			initial_parameters[i * n_model_parameters + p*4 + 2] =  creal(mp_profile->h[p]);
-			initial_parameters[i * n_model_parameters + p*4 + 3] =  cimag(mp_profile->h[p]);
+			// initial_parameters[i * n_model_parameters + p * 4 + 0] = mp_profile->tau[p]; // 4: t, v, h_real, h_imag
+			// initial_parameters[i * n_model_parameters + p * 4 + 1] = mp_profile->nu[p];
+			// initial_parameters[i * n_model_parameters + p * 4 + 2] = creal(mp_profile->h[p]);
+			// initial_parameters[i * n_model_parameters + p * 4 + 3] = cimag(mp_profile->h[p]);
+
+			initial_parameters[i * n_model_parameters + p * 4 + 0] = 0.5; // 4: t, v, h_real, h_imag
+			initial_parameters[i * n_model_parameters + p * 4 + 1] = 0.5;
+			initial_parameters[i * n_model_parameters + p * 4 + 2] = 0.5;
+			initial_parameters[i * n_model_parameters + p * 4 + 3] = 0.5;
 		}
 	}
 
 	// generate data
-	std::vector< REAL > data(2 * n_points_per_fit * n_fits);
-	for (std::size_t j = 0; j < n_fits; j++){
+	std::vector<REAL> data(2 * n_points_per_fit * n_fits);
+	for (std::size_t j = 0; j < n_fits; j++)
+	{
 		for (std::size_t i = 0; i < n_points_per_fit; i++)
 		{
-			data[j*n_fits+2*i] = creal(mp_config->y[i]);
-			data[j*n_fits+2*i+1] = cimag(mp_config->y[i]);
+			data[j * n_fits + 2 * i] = creal(mp_config->y[i]);
+			data[j * n_fits + 2 * i + 1] = cimag(mp_config->y[i]);
 		}
 	}
 
@@ -57,40 +63,38 @@ void mp_solver_example(mp_profile_t *mp_profile, mp_config_t *mp_config)
 	int const max_number_iterations = 20;
 
 	// estimator ID
-	int const estimator_id = LSE_COMPLEX;
+	int const estimator_id = MLE;
 
 	// model ID
 	int const model_id = CHANNEL_EQ;
 
 	// parameters to fit (all of them)
-	std::vector< int > parameters_to_fit(n_model_parameters, 1);
+	std::vector<int> parameters_to_fit(n_model_parameters, 1);
 
 	// output parameters
-	std::vector< REAL > output_parameters(n_fits * n_model_parameters);
-	std::vector< int > output_states(n_fits);
-	std::vector< REAL > output_chi_square(n_fits);
-	std::vector< int > output_number_iterations(n_fits);
+	std::vector<REAL> output_parameters(n_fits * n_model_parameters);
+	std::vector<int> output_states(n_fits);
+	std::vector<REAL> output_chi_square(n_fits);
+	std::vector<int> output_number_iterations(n_fits);
 
 	// call to gpufit (C interface)
-	int const status = gpufit
-        (
-            n_fits,
-            n_points_per_fit,
-            data.data(),
-            0,
-            model_id,
-            initial_parameters.data(),
-            tolerance,
-            max_number_iterations,
-            parameters_to_fit.data(),
-            estimator_id,
-            user_info_size,
-            reinterpret_cast< char * >( user_info.data() ),
-            output_parameters.data(),
-            output_states.data(),
-            output_chi_square.data(),
-            output_number_iterations.data()
-        );
+	int const status = gpufit(
+		n_fits,
+		n_points_per_fit,
+		data.data(),
+		0,
+		model_id,
+		initial_parameters.data(),
+		tolerance,
+		max_number_iterations,
+		parameters_to_fit.data(),
+		estimator_id,
+		user_info_size,
+		reinterpret_cast<char *>(user_info.data()),
+		output_parameters.data(),
+		output_states.data(),
+		output_chi_square.data(),
+		output_number_iterations.data());
 
 	// check status
 	if (status != ReturnState::OK)
@@ -98,25 +102,29 @@ void mp_solver_example(mp_profile_t *mp_profile, mp_config_t *mp_config)
 		throw std::runtime_error(gpufit_get_last_error());
 	}
 
-	//examine output_parameters
-	// Matrix[3x4]
-
+	// examine output_parameters
+	//  Matrix[3x4]
+	cout << "size=" << output_parameters.size() << endl;
+	for (int i = 0; i < 12; i++)
+	{
+		cout << output_parameters.data()[i] << endl;
+	}
 }
-
 
 int main(int argc, char *argv[])
 {
 	// read input
-    mp_profile_t mp_profile;
-    csvReader reader(CSV_PATH);
-    reader.readData();
-    mp_config_t mp_config = reader.getData();
-    
-    mp_solver_example(&mp_profile, &mp_config);
+	mp_profile_t mp_profile;
+	csvReader reader(CSV_PATH);
+	reader.readData();
+	mp_config_t mp_config = reader.getData();
 
-    std::cout << std::endl << "Example completed!" << std::endl;
-    std::cout << "Press ENTER to exit" << std::endl;
-    std::getchar();
-	
+	mp_solver_example(&mp_profile, &mp_config);
+
+	std::cout << std::endl
+			  << "Example completed!" << std::endl;
+	std::cout << "Press ENTER to exit" << std::endl;
+	std::getchar();
+
 	return 0;
 }
